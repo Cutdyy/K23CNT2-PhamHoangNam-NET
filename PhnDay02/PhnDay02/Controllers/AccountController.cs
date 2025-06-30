40f3de6ddfdef635860eaba49af6f9e558cbe8f9
Cutdyy <ngoisao1432@gmail.com>
2025-06-30 14:15:44 +0700

    PhnDay02

---
+++ PhnDay02/PhnDay02/Controllers/AccountController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using YourProjectName.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace YourProjectName.Controllers
+{
+    public class AccountController : Controller
+    {
+        private List<AccountViewModel> GetAccounts()
+        {
+            return new List<AccountViewModel>
+            {
+                new AccountViewModel { Id = 1, Name = "Nguyễn Văn A", Description = "Thành viên tích cực", AvatarPath = "~/Avatar/p1.png" },
+                new AccountViewModel { Id = 2, Name = "Trần Thị B", Description = "Quản trị viên", AvatarPath = "~/Avatar/p2.png" },
+                new AccountViewModel { Id = 3, Name = "Lê Văn C", Description = "Khách mới", AvatarPath = "~/Avatar/p3.png" }
+            };
+        }
+
+        public IActionResult Index()
+        {
+            var accounts = GetAccounts();
+            return View(accounts);
+        }
+
+        public IActionResult Profile(int id)
+        {
+            var account = GetAccounts().FirstOrDefault(a => a.Id == id);
+            if (account == null)
+                return NotFound();
+
+            return View(account);
+        }
+    }
+}
+
---
+++ PhnDay02/PhnDay02/Models/AccountViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace YourProjectName.Models
+{
+    public class AccountViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string AvatarPath { get; set; }
+    }
+}
